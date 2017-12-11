7b43438f08dba4342743b72d23ca6816e2fe7cb4
KangGrandesty <gangkanli1219@163.com>
2017-12-11 12:28:31 +0800

    uncomment

--- research/object_detection/beer/eval_tool/eval.sh
+++ research/object_detection/beer/eval_tool/eval.sh
@@ -11,12 +11,12 @@ output=$pretrained/$1
 DATA=/home/admins/data/beer_data/pre
 
 
-# python3 $DIR/../../object_detection/export_inference_graph.py \
-# --input_type image_tensor \
-# --pipeline_config_path $config \
-# --trained_checkpoint_prefix $pretrained/model.ckpt- \
-# --output_directory $pretrained/
-# python3 prediction.py
+ python3 $DIR/../../object_detection/export_inference_graph.py \
+ --input_type image_tensor \
+ --pipeline_config_path $config \
+ --trained_checkpoint_prefix $pretrained/model.ckpt- \
+ --output_directory $pretrained/
+ python3 prediction.py
 
 python3 $DIR/eval_tool/main.py \
 --target $DATA \
