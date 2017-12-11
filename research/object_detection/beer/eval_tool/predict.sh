77e313d02cf602b0da8d0e502a4068647a116cf0
KangGrandesty <gangkanli1219@163.com>
2017-12-11 13:12:51 +0800

    uncomment

--- research/object_detection/beer/eval_tool/predict.sh
+++ research/object_detection/beer/eval_tool/predict.sh
@@ -7,8 +7,8 @@ export PYTHONPATH=$PYTHONPATH:$DIR/../../object_detection
 
 DATA=/home/admins/data/beer_data
 
-#python3 $DIR/eval_tool/predict_images.py \
-#--image-list $DATA/pre.txt
+python3 $DIR/eval_tool/predict_images.py \
+--image-list $DATA/pre.txt
 
 python3 $DIR/eval_tool/predict_large_image.py \
 --image-path ceshi
